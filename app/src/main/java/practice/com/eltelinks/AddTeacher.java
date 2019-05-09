3ef778aff573c1f8b0b678bc420f78243a903d1b
Binnat Musayev <binnatmusayev@Binnats-MacBook-Pro.local>
2019-05-09 13:39:07 +0200

    loading website favicon

--- app/src/main/java/practice/com/eltelinks/AddTeacher.java
+++ app/src/main/java/practice/com/eltelinks/AddTeacher.java
@@ -2,10 +2,12 @@ package practice.com.eltelinks;
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,6 +22,7 @@ public class AddTeacher extends AppCompatActivity {
     private Button submit, cancel;
     private EditText name, email, faculty, website;
     private TeacherViewModel teacherViewModel;
+    Context c;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,15 +42,15 @@ public class AddTeacher extends AppCompatActivity {
 
         teacherViewModel = ViewModelProviders.of(this).get(TeacherViewModel.class);
 
-
+        c = getBaseContext();
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (name.getText().toString().isEmpty()
-                    || email.getText().toString().isEmpty()
-                    || faculty.getText().toString().isEmpty()
-                    || website.getText().toString().isEmpty()){
-                    Toast.makeText(AddTeacher.this, "Fill in all fields", Toast.LENGTH_SHORT);
+                if ( TextUtils.isEmpty(name.getText().toString().trim())
+                    || TextUtils.isEmpty(email.getText().toString().trim())
+                    || TextUtils.isEmpty(faculty.getText().toString().trim())
+                    || TextUtils.isEmpty(website.getText().toString().trim())){
+                    Toast.makeText(c, "Fill in all fields", Toast.LENGTH_SHORT).show();
                 }else{
                     Teacher t = new Teacher(name.getText().toString(),
                                                 email.getText().toString(),
@@ -56,7 +59,6 @@ public class AddTeacher extends AppCompatActivity {
 
                     teacherViewModel.addTeacher(t);
                     finish();
-
                 }
             }
         });
@@ -67,7 +69,6 @@ public class AddTeacher extends AppCompatActivity {
                 finish();
             }
         });
-
 
     }
 }
--- app/src/main/java/practice/com/eltelinks/adapters/WebsitesAdapter.java
+++ app/src/main/java/practice/com/eltelinks/adapters/WebsitesAdapter.java
@@ -1,12 +1,15 @@
 package practice.com.eltelinks.adapters;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import practice.com.eltelinks.R;
 import practice.com.eltelinks.model.Website;
@@ -49,8 +52,15 @@ public class WebsitesAdapter extends BaseAdapter {
         ImageView imageView = convertView.findViewById(R.id.website_logo);
         TextView textView = convertView.findViewById(R.id.website_title);
 
+
+        Drawable defaultLogo = context.getResources().getDrawable(R.drawable.temp_logo);
+        Glide.with(context)
+                .load("https://elte.hu/favicon.ico")
+                .centerCrop()
+                .error(defaultLogo)
+                .into(imageView);
+
         //later get these from database
-        imageView.setImageResource(R.drawable.logo);
         textView.setText(website.getTitle());
 
         return convertView;
